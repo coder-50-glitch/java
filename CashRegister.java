58ca3f636a2d0af33870baea271fd85970ba280b
coder-50-glitch <dharrajashree74@gmail.com>
2026-08-17 22:54:34 +0530

    Add files via upload

---
+++ CashRegister.java
@@ -0,0 +1,26 @@
+public class CashRegister {
+    private double purchase;
+    private double payment;
+
+    public CashRegister() {
+        purchase = 0;
+        payment = 0;
+    }
+
+    public void recordPurchase(double amount) {
+        purchase += amount;
+    }
+
+    public void receivePayment(double amount) {
+        payment += amount;
+    }
+
+    public double giveChange() {
+        double change = payment - purchase;
+        purchase = 0;
+        payment = 0;
+        return change;
+    }
+
+    // additional methods not shown
+}
